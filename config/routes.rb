fa2bcc24828870c97fca2c7eb69a7c18f6496e0d
Lyn <lynfinman@gmail.com>
2022-08-04 16:29:25 -0400

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  resources :routines
-  resources :exercises
+  resources :routines, only: %i[show index]
+  resources :exercises, only: %i[show index]
   resources :workouts
-  resources :users
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get '*path', to: 'fallback#index', constraints: ->(req) { !req.xhr? && req.format.html? }
@@ -10,7 +9,4 @@ Rails.application.routes.draw do
   post '/signup', to: 'users#create'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  get '/workouts', to: 'workouts#index'
-  post '/workouts', to: 'workouts#create'
-  delete '/workouts', to: 'workouts#destroy'
 end
