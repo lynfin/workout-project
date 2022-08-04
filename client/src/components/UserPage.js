511de42780006b2461b30ea22650f68f449a1566
aaron wertheim <aawertheim@gmail.com>
2022-08-04 12:03:03 -0500

    user page sizing

--- client/src/components/UserPage.js
+++ client/src/components/UserPage.js
@@ -6,24 +6,27 @@ import styled from "styled-components";
 const UserPage = ({ user }) => {
 
   return (
-    <div style={{width: '100%', background: '#d3d3d3', display: 'flex', overflowX: 'hidden'}}> 
-      <div style={{ 
-        minHeight: '100vh',
-        borderRight: 'solid black 3px',
-        background: 'white',
-        minWidth: '400px',
-        textAlign: 'justify',
-        padding:'10px'}}
-      >
-        <h2 style={{ textAlign: "center" }}>{user.username}</h2>
-        <img
-          style={{ width: '100%'}}
-          src={user.image_url}
-          alt="img"
-        ></img>
-        <h3 style={{ fontSize: '18px' }}>{user.bio}</h3>
-      </div>
-      <div ><img style={{minHeight: '100vh'}} src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3ltfGVufDB8fDB8fA%3D%3D&w=1000&q=80"></img></div>
+    <div style={{width: '100%', background: 'black', display: 'flex', overflow: 'hidden'}}> 
+        <div style={{ 
+          height: '100vh',
+          background: '#d3d3d3',
+          minWidth: '400px',
+          textAlign: 'justify',
+          padding:'10px',
+          overflowY: 'scroll'
+        }}
+        >
+          <h2 style={{ textAlign: "center" }}>{user.username}</h2>
+          <img
+            style={{ width: '100%'}}
+            src={user.image_url}
+            alt="img"
+          ></img>
+          <h3 style={{ fontSize: '18px' }}>{user.bio}</h3>
+        </div>
+        <div >
+          <img style={{height: '100vh'}} src="https://images.unsplash.com/photo-1571902943202-507ec2618e8f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Z3ltfGVufDB8fDB8fA%3D%3D&w=1000&q=80"></img>
+        </div>
     </div>
   );
 };
